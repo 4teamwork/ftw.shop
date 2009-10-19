b793f187bf8dd190c8c4d496d1648b3d9dae3fcc
Thomas Buchberger <t.buchberger@4teamwork.ch>
2009-10-19 16:41:14 +0000

    added edit form for categories

---
+++ amnesty/shop/skins/amnesty_shop/listAllCategories.py
@@ -0,0 +1,23 @@
+## Script (Python) "listAllCategories"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=categoryUID=None
+##title=
+##
+# return all Category instances except the context
+# (that could be a Category instance too).
+from Products.CMFCore.utils import getToolByName
+
+portalPath = getToolByName(context, 'portal_url').getPortalPath()
+ltool = getToolByName(context, 'portal_languages')
+lang = ltool.getPreferredLanguage()
+
+query = {}
+query['portal_type'] = ['ShopCategory']
+query['path'] = '%s/%s' % (portalPath, lang) 
+
+categories = context.portal_catalog.queryCatalog(query)
+return [category.getObject() for category in categories if category.UID != categoryUID]
---
+++ amnesty/shop/skins/amnesty_shop/update_categories.py
@@ -0,0 +1,33 @@
+## Script (Python) "update_categories"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=categories=[]
+##title=
+##
+from Products.CMFCore.utils import getToolByName
+
+atool = context.archetype_tool
+old_uids = [obj.UID() for obj in context.listCategories()]
+
+# add the checked categories
+new_uids = [atool.getObject(uid) for uid in categories if uid not in old_uids]
+for c in new_uids:
+    context.addToCategory(c)
+
+# remove the unchecked categories
+uids_delete = [uid for uid in old_uids if uid not in categories]
+categories_delete = [atool.getObject(uid) for uid in uids_delete]
+for c in categories_delete:
+    context.removeFromCategory(c)
+  
+#update ranks
+for c in context.listCategories():
+    rank = context.REQUEST.get('rank_%s' % c.UID(), 1)
+    context.setRankForCategory(c, rank)
+
+putils = getToolByName(context, 'plone_utils')
+putils.addPortalMessage('Categories updated.', 'info')
+return context.REQUEST.RESPONSE.redirect('%s' % (context.absolute_url()))
