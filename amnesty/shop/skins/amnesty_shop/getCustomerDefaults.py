533600b6f2b056a4b703e52ed85ceaa872ae3c8b
Thomas Buchberger <t.buchberger@4teamwork.ch>
2009-11-03 20:48:50 +0000

    cleaned up method

--- amnesty/shop/skins/amnesty_shop/getCustomerDefaults.py
+++ amnesty/shop/skins/amnesty_shop/getCustomerDefaults.py
@@ -18,70 +18,13 @@ v = r.get(field, None)
 if v: return v
 
 # next, look in SESSION
-if field in ['title',
-             'firstname',
-             'lastname',
-             'newsletter',
-             'email',
-             'phone',
-             'becomeAMember',
-             'activeMember',
-             'group',
-             'comment'] and s.has_key('customer'):
-    v = s['customer'].get(field, None)
-    if v: return v
-    
-elif field in ['location_name',
-               'street',
-               'street_no',
-               'street2',
-               'zipcode',
-               'city',] and s.has_key('customer_address'):
-    v = s['customer_address'].get(field, None)
-    if v: return v
-
-# next, look at Member folder by CustomerInformation and
-# AddressInformation objects
-
-pm = context.portal_membership
-m = pm.getAuthenticatedMember()
-mf = pm.getHomeFolder()
-
-# TODO: It's HARDCODED and need to be refactored to something more
-# generic, following the interfaces register approach.
-
-ci = getattr(mf, 'customer_information', None)
-if ci:
-    d = {'firstname' : 'getFirstname',
-         'lastname'  : 'getLastname',
-         'company'   : 'getCompany',
-         'email'     : 'getEmail',
-         'phoneNumbers':'getPhoneNumbers',
-         'EUVATId':'getEUVATId',
-         'comment':'getComment'
-         }
-         
-    
-    for k, v in d.items():
-        if k == field:
-            res=getattr(ci, v)()
-            return res
-
-    # ugly, but works!
-    ai = ci.address_information_set.address_information
-    if ai:
-        d = {'location_name' : 'getLocation_name',
-             'address1'      : 'getAddress1',
-             'address2'      : 'getAddress2',
-             'postal_code'   : 'getPostal_code',
-             'city'          : 'getCity',
-             'state'         : 'getState',
-             'country'       : 'getCountry'}
-        for k, v in d.items():
-            if k == field:
-                return getattr(ai, v)()
+customer = s.get('customer_data', {})
+v = customer.get(field, None)
+if v: return v
 
 # last, look member preferences
+pm = context.portal_membership
+m = pm.getAuthenticatedMember()
 if field in ['firstname', 'lastname', 'email']:
     if field == 'email':
         return m.getProperty('email', "")
