a2dff9b6d6ad803c93bf3692393866f2534254d5
Lukas Graf <lukas.graf@4teamwork.ch>
2010-09-28 16:27:01 +0200

    Base ShopCategories on ATFolder, not ATDocument (d'oh)

--- ftw/shop/content/shopcategory.py
+++ ftw/shop/content/shopcategory.py
@@ -5,18 +5,18 @@ from zope.interface import implements
 from Products.Archetypes import atapi
 # from amnesty.base.content.aiarticle import AIArticle
 # from amnesty.base.content.aiarticle import AIArticleSchema
-from Products.ATContentTypes.content.document import ATDocument
-from Products.ATContentTypes.content.document import ATDocumentSchema
+from Products.ATContentTypes.content.folder import ATFolder
+from Products.ATContentTypes.content.folder import ATFolderSchema
 from ftw.shop.interfaces.shopcategory import IShopCategory
 from ftw.shop.config import PROJECTNAME
 from ftw.shop.content.categorizeable import Categorizeable
 
 
-class ShopCategory(Categorizeable, ATDocument):
+class ShopCategory(Categorizeable, ATFolder):
     """A category for shop items"""
     implements(IShopCategory)
 
     meta_type = "ShopCategory"
-    schema = ATDocumentSchema.copy()
+    schema = ATFolderSchema.copy()
 
 atapi.registerType(ShopCategory, PROJECTNAME)
