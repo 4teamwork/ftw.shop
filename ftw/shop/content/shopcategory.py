1c579098aa956895f4bc419bd1379f2602fbd96c
Lukas Graf <lukas.graf@4teamwork.ch>
2010-10-14 11:45:24 +0200

    Added event handler for ShopCategory to add to contained category upon creation

--- ftw/shop/content/shopcategory.py
+++ ftw/shop/content/shopcategory.py
@@ -1,10 +1,14 @@
 """Definition of the Shop Category content type
 """
 
+from Acquisition import aq_parent
+
 from zope.interface import implements
+
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content.folder import ATFolder
 from Products.ATContentTypes.content.folder import ATFolderSchema
+
 from ftw.shop.interfaces import IShopCategory
 from ftw.shop.config import PROJECTNAME
 from ftw.shop.content.categorizeable import Categorizeable
@@ -18,3 +22,14 @@ class ShopCategory(Categorizeable, ATFolder):
     schema = ATFolderSchema.copy()
 
 atapi.registerType(ShopCategory, PROJECTNAME)
+
+def object_initialized_handler(context, event):
+    """
+    @param context: Zope object for which the event was fired for. Usually this is Plone content object.
+
+    @param event: Subclass of event.
+    """
+    parent = aq_parent(context)
+    import pdb; pdb.set_trace()
+    if parent.portal_type == 'ShopCategory':
+        context.addToCategory(parent.UID())
