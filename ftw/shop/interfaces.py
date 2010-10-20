27fdb54a6be1a1a4f9b557fe4e71e6de3b68425d
Lukas Graf <lukas.graf@4teamwork.ch>
2010-10-20 10:39:51 +0200

    Added field to select enabled payment processors to configlet

--- ftw/shop/interfaces.py
+++ ftw/shop/interfaces.py
@@ -1,16 +1,8 @@
 from ftw.shop import shopMessageFactory as _
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope import schema
-from zope.schema import vocabulary
-from zope.app.container.constraints import containers, contains
 from zope.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from z3c.form import interfaces
-from plone.app.form.widgets import MultiCheckBoxWidget
-
-
 
 
 class IShopRoot(Interface):
@@ -90,32 +82,37 @@ class IOrderReviewStep(IWizardStep):
 class IShopConfiguration(Interface):
     """This interface defines the ftw.shop configlet."""
 
-    shop_name = schema.TextLine(title=_(u"label_shop_name", default=u"Enter the shop name"),
-                                  required=True)
+    shop_name = schema.TextLine(
+            title=_(u"label_shop_name", default=u"Enter the shop name"),
+            required=True)
 
-    payment_processors = schema.List(title=_(u"label_payment_processors", default="Payment Processors"),
-                                      value_type=schema.Choice(vocabulary="ftw.shop.payment_processors"),
-                                    )
+    enabled_payment_processors = schema.List(
+            title=_(u"label_enabled_payment_processors", 
+                    default="Enabled Payment Processors"),
+            value_type=schema.Choice(vocabulary="ftw.shop.payment_processors"),)
 
-    contact_info_step_group = schema.Choice(title=_(u"label_contact_info_step_group", default="Contact Information Step Group"),
-                                      vocabulary="ftw.shop.contact_info_step_groups",
-                                      required=True)
+    contact_info_step_group = schema.Choice(
+            title=_(u"label_contact_info_step_group", 
+                    default="Contact Information Step Group"),
+            vocabulary="ftw.shop.contact_info_step_groups",
+            required=True)
     
 
 class IDefaultPaymentProcessorChoice(Interface):
     """Schema defining a choice from available payment processors
     """
-    payment_processor = schema.Choice(title=_(u"label_payment_processor", default="Payment Processor"),
-                                      vocabulary="ftw.shop.payment_processors",
-                                      required=True,
-                                    )
+    payment_processor = schema.Choice(
+            title=_(u"label_payment_processor", default="Payment Processor"),
+            vocabulary="ftw.shop.enabled_payment_processors",
+            required=True)
     
 class IDefaultPaymentProcessorDetails(Interface):
     """Schema defining details for a chosen payment processor
     """
-    details = schema.TextLine(title=_(u"label_payment_processor_details", default="Payment Processor Details"),
-                                      required=True,
-                                    )
+    details = schema.TextLine(
+            title=_(u"label_payment_processor_details", 
+                    default="Payment Processor Details"),
+            required=True)
 
 
 
@@ -123,53 +120,52 @@ class IDefaultContactInformation(Interface):
     """Schema defining a common contact address form
     """
     title= schema.TextLine(
-      title=_(u'label_title', default=u'Title'),
-      required=True)
+            title=_(u'label_title', default=u'Title'),
+            required=True)
 
     firstname = schema.TextLine(
-      title=_(u'label_firstname', default=u'First Name'),
-      required=True)
+            title=_(u'label_firstname', default=u'First Name'),
+            required=True)
 
     lastname = schema.TextLine(
-      title=_(u'label_lastname', default=u'Last Name'),
-      required=True)
+            title=_(u'label_lastname', default=u'Last Name'),
+            required=True)
 
     email = schema.TextLine(
-      title=_(u'label_email', default=u'Email'),
-      required=True)
+            title=_(u'label_email', default=u'Email'),
+            required=True)
 
     street = schema.TextLine(
-      title=_(u'label_street', default=u'Street/No.'),
-      required=True)
+            title=_(u'label_street', default=u'Street/No.'),
+            required=True)
 
     street2 = schema.TextLine(
-      title=_(u'label_street2', default=u'Address 2'),
-      required=False)
+            title=_(u'label_street2', default=u'Address 2'),
+            required=False)
 
     phone = schema.TextLine(
-      title=_(u'label_phone_number', default=u'Phone number'),
-      required=True)
+            title=_(u'label_phone_number', default=u'Phone number'),
+            required=True)
 
     zipcode = schema.Int(
-      title=_(u'label_zipcode', default=u'Zip Code'),
-      required=True)
+            title=_(u'label_zipcode', default=u'Zip Code'),
+            required=True)
 
     city = schema.TextLine(
-      title=_(u'label_city', default=u'City'),
-      required=True)
+            title=_(u'label_city', default=u'City'),
+            required=True)
 
     country = schema.TextLine(
-      title=_(u'label_country', default=u'Country'),
-      required=True)
+            title=_(u'label_country', default=u'Country'),
+            required=True)
 
     newsletter = schema.Bool(
-      title=_(u'label_newsletter', default=u'Newsletter'),
-      description=_(u'help_newsletter', default=u'I would like to subscribe to the newsletter.'),
-      required=False,
-      default=False)
+            title=_(u'label_newsletter', default=u'Newsletter'),
+            description=_(u'help_newsletter',
+                          default=u'I would like to subscribe to the newsletter.'),
+            required=False,
+            default=False)
 
     comments = schema.Text(
-      title=_(u'label_comments', default=u'Comments'),
-      required=False)
-
-
+            title=_(u'label_comments', default=u'Comments'),
+            required=False)
--- ftw/shop/vocabularies.py
+++ ftw/shop/vocabularies.py
@@ -1,11 +1,12 @@
 from zope.schema import vocabulary
 from zope.schema.interfaces import IVocabularyFactory
-from zope.component import getAdapters
+from zope.component import getAdapters, getUtility
 from zope.interface import directlyProvides
+from plone.registry.interfaces import IRegistry
 
 from ftw.shop.interfaces import IContactInformationStepGroup
 from ftw.shop.interfaces import IPaymentProcessor
-
+from ftw.shop.interfaces import IShopConfiguration
 
 def ContactInfoStepGroups(context):
     # context is the portal config options, whose context is the portal
@@ -20,3 +21,14 @@ def PaymentProcessors(context):
     processor_names = set( map(unicode, [ n for n,a in payment_processors]) )
     directlyProvides(PaymentProcessors, IVocabularyFactory)
     return vocabulary.SimpleVocabulary.fromValues(processor_names)
+
+def EnabledPaymentProcessors(context):
+    # context is the portal config options, whose context is the portal
+    payment_processors = getAdapters((context, None, context,), IPaymentProcessor)
+    registry = getUtility(IRegistry)
+    shop_config = registry.forInterface(IShopConfiguration)
+    processor_names = set( map(unicode, [ n for n,a in payment_processors]) )
+    enabled_processor_names = [pn for pn in processor_names 
+                               if pn in shop_config.enabled_payment_processors]
+    directlyProvides(EnabledPaymentProcessors, IVocabularyFactory)
+    return vocabulary.SimpleVocabulary.fromValues(enabled_processor_names)
