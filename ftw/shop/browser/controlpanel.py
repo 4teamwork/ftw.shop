ab91b04182e9d0afe2310733e8f1453ce7c213c9
zope <zope@tau.4teamwork.ch>
2010-10-21 11:55:13 +0200

    Plone 3 BBB: Override template on form wrapper, not form itself

--- ftw/shop/browser/controlpanel.py
+++ ftw/shop/browser/controlpanel.py
@@ -1,9 +1,16 @@
 from zope.formlib import form
 
-#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from plone.app.registry.browser import controlpanel
+try:
+    # plone.app.registry 1.0b1
+    from plone.app.registry.browser.form import RegistryEditForm
+    from plone.app.registry.browser.form import ControlPanelFormWrapper
+except ImportError:
+    # plone.app.registry 1.0b2+
+    from plone.app.registry.browser.controlpanel import RegistryEditForm
+    from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+
 
 from ftw.shop.interfaces import IShopConfiguration
 from ftw.shop import shopMessageFactory as _
@@ -12,19 +19,20 @@ from zope.app.form.browser import MultiSelectWidget
 from plone.app.z3cform.layout import wrap_form, FormWrapper
 
 
-class ShopConfigurationForm(controlpanel.RegistryEditForm):
+class ShopConfigurationForm(RegistryEditForm):
     """Configuration form for the ftw.shop configlet
     """
     
     schema = IShopConfiguration
     form_fields = form.Fields(IShopConfiguration)
     label = _(u'label_shop_configuration', default=u"Shop configuration")
-    template = ViewPageTemplateFile('templates/controlpanel.pt')
+    #template = ViewPageTemplateFile('templates/controlpanel.pt')
        
     def updateFields(self):
         super(ShopConfigurationForm, self).updateFields()
 
     def updateWidgets(self):
         super(ShopConfigurationForm, self).updateWidgets()
-        
-ShopConfigurationView = wrap_form(ShopConfigurationForm)
+
+ShopConfigurationView = wrap_form(ShopConfigurationForm, ControlPanelFormWrapper)
+ShopConfigurationView.template = ViewPageTemplateFile('templates/controlpanel.pt')
