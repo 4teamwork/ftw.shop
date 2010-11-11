5f480cbddedc3a578423d6f0f30cdc0f0e5534b0
Lukas Graf <lukas.graf@4teamwork.ch>
2010-11-11 12:37:57 +0100

    Added test suite for utils module

---
+++ ftw/shop/tests/test_utils.py
@@ -0,0 +1,23 @@
+import unittest
+from decimal import Decimal
+from ftw.shop.utils import to_decimal
+
+from ftw.shop.tests.base import FtwShopTestCase
+
+
+class TestUtils(FtwShopTestCase):
+    
+    def afterSetUp(self):
+        super(TestUtils, self).afterSetUp()
+ 
+    def test_to_decimal(self):
+        self.assertEquals(to_decimal('10.00'), Decimal('10.00'))
+        self.assertEquals(to_decimal('123'), Decimal('123.00'))
+        self.assertEquals(to_decimal('50.25999'), Decimal('50.25'))
+        self.assertEquals(to_decimal('NaN'), 'NaN')
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestUtils))
+    return suite
--- ftw/shop/utils.py
+++ ftw/shop/utils.py
@@ -3,8 +3,10 @@ from decimal import Decimal, InvalidOperation
 def to_decimal(number):
     """Since SQLite doesn't support Decimal fields, trim the float it
     returns to two decimal places and convert it to Decimal. If that
-    fails, return the total as-is."""
+    fails, return the number as-is."""
     try:
+        if str(number).find('.') == -1:
+            return Decimal("%s.00" % number)
         return Decimal(str(number)[:str(number).find('.') + 3])
     except InvalidOperation:
         return number
