d882c0e7a0d600946a7130c0705dff6fe5cf13e1
Lukas Graf <lukas.graf@4teamwork.ch>
2010-11-19 13:46:05 +0100

    Updated docstring

--- ftw/shop/root.py
+++ ftw/shop/root.py
@@ -5,9 +5,12 @@ from Products.CMFCore.interfaces import ISiteRoot
 
 
 def get_shop_root_object(context):
-    """ return the shop root object.
-        if no shop root is defined, return the navigation root or the site
+    """ Return the shop root object.
+        If no shop root is defined, return the navigation root or the site
         root.
+        Since this method traverses up the site until it finds an IShopRoot,
+        it also works correctly on a site using LinguaPlone where there is
+        one ShopRoot per language branch.
     """
     obj = aq_inner(context)
     while not IShopRoot.providedBy(obj) \
