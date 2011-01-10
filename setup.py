d0a4f08560807f49d04294693b375d9d81e5a69b
Lukas Graf <lukas.graf@4teamwork.ch>
2011-01-10 09:27:37 +0100

    Preparations for tagging 1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0'
+version = open('ftw/shop/version.txt').read().strip()
 
 long_description = (
     read('README.txt')
