0c9b7a6a7d086faaa4899e8b650e0ebae6ec484a
Lukas Graf <lukas.graf@4teamwork.ch>
2011-01-11 17:52:02 +0100

    Enabled use of custom prefilling JS for contact form

---
+++ ftw/shop/browser/resources/contactform_prefill.js
@@ -0,0 +1,6 @@
+jq(function () {
+    // Load contact info depending on personnel no.
+    jq('input#contact_information-widgets-personnel_no').blur(function (event) {
+        // Do something here
+    });
+});
