067cfef07b2962eeec791be00b06a0116618027e
Thomas Buchberger <t.buchberger@4teamwork.ch>
2011-12-27 11:15:30 +0100

    Cleanup of package and history.

--- setup.py
+++ setup.py
@@ -5,41 +5,15 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = open('ftw/shop/version.txt').read().strip()
-
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('ftw', 'shop', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
+version = '1.1dev'
 
 tests_require=['zope.testing']
 
 setup(name='ftw.shop',
       version=version,
       description="A web shop solution for Plone",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         'Framework :: Plone',
         'Intended Audience :: Developers',
@@ -72,6 +46,5 @@ setup(name='ftw.shop',
       # -*- entry_points -*-
       [z3c.autoinclude.plugin]
       target = plone
-
       """,
       )
