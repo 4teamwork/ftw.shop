fd27e8b46b845cfa27dd86e300291e6e44a18a94
Julian Infanger <j.infanger@4teamwork.ch>
2012-09-26 09:13:28 +0200

    Removed unused imports. Cleanup whitespaces.

--- ftw/shop/tests/test_checkout.py
+++ ftw/shop/tests/test_checkout.py
@@ -1,14 +1,9 @@
 import unittest
-import simplejson
-import base64
 
 from ftw.shop.browser.checkout import CheckoutView
 from ftw.shop.browser.checkout import CheckoutWizard
-from ftw.shop.browser.checkout import DefaultContactInfoStep
 from ftw.shop.browser.cart import CartView
-from ftw.shop.config import SESSION_ADDRESS_KEY, COOKIE_ADDRESS_KEY
 from ftw.shop.tests.base import FtwShopTestCase
-from ftw.shop.tests.base import MOCK_CUSTOMER
 
 
 class TestCheckout(FtwShopTestCase):
@@ -17,7 +12,7 @@ class TestCheckout(FtwShopTestCase):
         super(TestCheckout, self).afterSetUp()
         self.checkout_view = CheckoutView(self.portal, self.portal.REQUEST)
         self.wizard = CheckoutWizard(self.portal, self.portal.REQUEST)
- 
+
     def test_checkout_view(self):
         self.assertTrue(isinstance(self.checkout_view, CheckoutView))
         self.assertTrue(isinstance(self.portal.REQUEST['cart_view'], CartView))
@@ -30,34 +25,34 @@ class TestCheckout(FtwShopTestCase):
         default_payment_processor_choice_step = steps[2]
         default_order_review_step = steps[3]
 
-        self.assertEquals(default_contact_info_step.title, 
+        self.assertEquals(default_contact_info_step.title,
                           u'title_default_contact_info_step')
-        self.assertEquals(default_shipping_address_step.title, 
+        self.assertEquals(default_shipping_address_step.title,
                         u'title_default_shipping_address_step')
-        self.assertEquals(default_payment_processor_choice_step.title, 
+        self.assertEquals(default_payment_processor_choice_step.title,
                           u'title_default_payment_processor_step')
-        self.assertEquals(default_order_review_step.title, 
+        self.assertEquals(default_order_review_step.title,
                           u'title_default_order_review_step')
 
     # def test_default_contact_info_step(self):
     #     wizard = self.wizard
     #     session = self.portal.REQUEST.SESSION
-    # 
+    #
     #     # Test form prefill from data in session
     #     session[SESSION_ADDRESS_KEY] = MOCK_CUSTOMER
-    # 
-    #     step = DefaultContactInfoStep(self.portal, 
+    #
+    #     step = DefaultContactInfoStep(self.portal,
     #                                   self.portal.REQUEST,
     #                                   wizard)
     #     for key in MOCK_CUSTOMER.keys():
     #         self.assertEquals(step.fields[key].field.default,
     #                           MOCK_CUSTOMER[key])
-    # 
-    # 
+    #
+    #
     #     # Test form prefill from cookie
     #     cookie_data = base64.b64encode(simplejson.dumps(MOCK_CUSTOMER))
     #     self.portal.REQUEST[COOKIE_ADDRESS_KEY] = cookie_data
-    #     step = DefaultContactInfoStep(self.portal, 
+    #     step = DefaultContactInfoStep(self.portal,
     #                                   self.portal.REQUEST,
     #                                   wizard)
     #     for key in MOCK_CUSTOMER.keys():
