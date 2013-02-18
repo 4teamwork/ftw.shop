8631326ad13c64978d6f4040646d263e1b3abe08
Jonas Baumann <jone@jone.ch>
2013-02-18 15:10:22 +0100

    Cleanup (imports, indents)

--- ftw/shop/content/supplier.py
+++ ftw/shop/content/supplier.py
@@ -1,11 +1,12 @@
-"""Definition of the Supplier content type
-"""
-
-from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content.schemata import ATContentTypeSchema
+from Products.Archetypes import atapi
 from Products.Archetypes.Widget import TextAreaWidget
+from ftw.shop import shopMessageFactory as _
+from ftw.shop.config import PROJECTNAME
+from ftw.shop.interfaces import ISupplier
 from zope.interface import implements
+
 
 from Products.ATContentTypes.config import HAS_LINGUA_PLONE
 if HAS_LINGUA_PLONE:
@@ -13,32 +14,30 @@ if HAS_LINGUA_PLONE:
 else:
     from Products.Archetypes.atapi import registerType
 
-from ftw.shop.interfaces import ISupplier
-from ftw.shop.config import PROJECTNAME
-from ftw.shop import shopMessageFactory as _
 
 SupplierSchema = ATContentTypeSchema.copy() + atapi.Schema((
 
-    atapi.TextField(
-        'email',
-        required=True,
-        label=_(u'label_supplier_email', default=u"E-Mail")
-    ),
-    atapi.TextField(
-        'address',
-        required=False,
-        widget=TextAreaWidget,
-        label=_(u'label_supplier_address', default=u"Address"),
-        description=_(u'help_supplier_address',
-                      default=u"Supplier's postal address")
-    ),
-))
+        atapi.TextField(
+            name='email',
+            required=True,
+            label=_(u'label_supplier_email', default=u"E-Mail")),
+
+        atapi.TextField(
+            name='address',
+            required=False,
+
+            widget=TextAreaWidget(
+                label=_(u'label_supplier_address', default=u"Address"),
+                description=_(u'help_supplier_address',
+                              default=u"Supplier's postal address")))))
+
 
 SupplierSchema['title'].widget.label = _(u"label_name", default="Name")
-SupplierSchema['title'].widget.description = _(u"help_name", default="The supplier's name")
+SupplierSchema['title'].widget.description = _(u"help_name",
+                                               default="The supplier's name")
 
-SupplierSchema['description'].widget.visible = {'edit': 'invisible', 'view': 'invisible'}
-
+SupplierSchema['description'].widget.visible = {'edit': 'invisible',
+                                                'view': 'invisible'}
 
 
 class Supplier(base.ATCTContent):
