e1f1dc156360763a4545a538ac074122f94e3ed4
Thomas Buchberger <t.buchberger@4teamwork.ch>
2013-09-05 14:35:39 +0200

    Preparing release 1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '1.2.1.dev0'
+version = '1.3'
 
 tests_require = ['zope.testing',
                  'Products.PloneTestCase',
