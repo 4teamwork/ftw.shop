ee8cda562057e24ddc2d2bf091b8098252b51600
Thomas Buchberger <t.buchberger@4teamwork.ch>
2014-02-19 10:27:47 +0100

    Back to development: 1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '1.3.1'
+version = '1.3.2.dev0'
 
 tests_require = ['zope.testing',
                  'Products.PloneTestCase',
