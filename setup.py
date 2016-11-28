64b19b31f209333d330add56ad41a9a47565815a
raphael-s <r.schwob@4teamwork.ch>
2016-11-28 13:50:24 +0100

    Preparing release 2.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '2.0.10.dev0'
+version = '2.0.10'
 
 tests_require = [
     'Products.PloneTestCase',
