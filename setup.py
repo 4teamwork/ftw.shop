1aee00e0311437a307fcd2185852c4b605b5bda4
raphael-s <r.schwob@4teamwork.ch>
2016-11-28 13:52:52 +0100

    Back to development: 2.0.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '2.0.10'
+version = '2.0.11.dev0'
 
 tests_require = [
     'Products.PloneTestCase',
