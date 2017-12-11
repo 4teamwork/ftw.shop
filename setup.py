e94f4d98cbf273a5ceee140d0360ae6257558a56
Jonas Baumann <jone@jone.ch>
2017-12-11 23:12:59 +0100

    Back to development: 2.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '2.3.2'
+version = '2.3.3.dev0'
 
 tests_require = [
     'Products.PloneTestCase',
