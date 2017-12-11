b76d5e41c7f345e61d393b5a6d2e8c1a71760242
Jonas Baumann <jone@jone.ch>
2017-12-11 23:12:41 +0100

    Preparing release 2.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of ftw.shop
 import os
 from setuptools import setup, find_packages
 
-version = '2.3.2.dev0'
+version = '2.3.2'
 
 tests_require = [
     'Products.PloneTestCase',
